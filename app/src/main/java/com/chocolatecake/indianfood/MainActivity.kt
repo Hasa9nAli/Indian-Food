7561af24ed1617b360045b4f24dc893024101b8b
hawraamahmoud <hawraaalobaidy2000@gmail.com>
2023-03-22 16:48:34 +0300

    add categories item

--- app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
+++ app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-
+     val myFirstFragment=CategoriesFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initSubView()
+    }
+
+    private fun initSubView() {
+       val transaction= supportFragmentManager.beginTransaction()
+        transaction.add(R.id.fragment_categories,myFirstFragment)
+        transaction
     }
 }
