1451d5ee0bcdd18b113db24785dce57b0ca7b167
Asia sama <sehunexo710@gmail.com>
2023-03-22 19:25:03 +0300

    fix bugs

--- app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
+++ app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    val onBaording = OnBoardingFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val trans = supportFragmentManager.beginTransaction()
+        trans.add(R.id.container_fragment, onBaording)
+        trans.commit()
     }
 }
