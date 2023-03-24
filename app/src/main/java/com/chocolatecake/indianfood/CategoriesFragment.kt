be7ea119075b3126f88e1bf2fca81788917158a7
hawraamahmoud <hawraaalobaidy2000@gmail.com>
2023-03-24 08:50:14 +0300

    complete xml

--- app/src/main/java/com/chocolatecake/indianfood/CategoriesFragment.kt
+++ app/src/main/java/com/chocolatecake/indianfood/CategoriesFragment.kt
@@ -5,17 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chocolatecake.indianfood.databinding.FragmentCategoriesBinding
 
 
 class CategoriesFragment : Fragment() {
+ private lateinit var  _binding:FragmentCategoriesBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding =FragmentCategoriesBinding.inflate(inflater, container, false)
+        return _binding.root
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.item_categories, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding.cardView1.setOnClickListener { }
     }
 
 
--- app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
+++ app/src/main/java/com/chocolatecake/indianfood/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.chocolatecake.indianfood
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 
 
 class MainActivity : AppCompatActivity() {
-     val myFirstFragment=CategoriesFragment()
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initSubView()
+        supportFragmentManager.beginTransaction()  .add(R.id.fragment_container,CategoriesFragment()).commit()
+
+
     }
 
-    private fun initSubView() {
-       val transaction= supportFragmentManager.beginTransaction()
-        transaction.add(R.id.fragment_categories,myFirstFragment)
-        transaction
-    }
+
 }
