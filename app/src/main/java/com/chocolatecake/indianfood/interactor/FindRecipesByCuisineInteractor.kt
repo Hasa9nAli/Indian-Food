87a7d2bcb4a44e1a61cfdb3b512c393bb3d190f1
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-03-24 13:57:57 +0200

    change access modifier

--- app/src/main/java/com/chocolatecake/indianfood/interactor/FindRecipesByCuisineInteractor.kt
+++ app/src/main/java/com/chocolatecake/indianfood/interactor/FindRecipesByCuisineInteractor.kt
@@ -3,14 +3,15 @@ package com.chocolatecake.indianfood.interactor
 import com.chocolatecake.indianfood.model.Recipe
 
 class FindRecipesByCuisineInteractor(private val dataSource: IndianFoodDataSource){
-    fun invoke(cuisine: String): List<Recipe>{
+    fun invoke(cuisine: String): List<Recipe> {
         return getRecipesBuyCuisineName(cuisine)
     }
-    fun getRecipesBuyCuisineName(cuisine: String): List<Recipe>{
+
+    private fun getRecipesBuyCuisineName(cuisine: String): List<Recipe> {
         return dataSource
             .getAllRecipesData()
             .ifEmpty { throw IllegalStateException("Something went wrong") }
-            .filter { recipe -> recipe.cuisine.lowercase().trim() == cuisine.lowercase().trim()  }
+            .filter { recipe -> recipe.cuisine.lowercase().trim() == cuisine.lowercase().trim() }
     }
 
 }
