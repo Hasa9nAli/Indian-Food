ed0982f6fc2ec5c600c6fb967e8013d514013931
hawraamahmoud <hawraaalobaidy2000@gmail.com>
2023-03-25 00:05:29 +0300

    edit names card views

--- app/src/main/java/com/chocolatecake/indianfood/CategoriesFragment.kt
+++ app/src/main/java/com/chocolatecake/indianfood/CategoriesFragment.kt
@@ -6,23 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.chocolatecake.indianfood.databinding.FragmentCategoriesBinding
+import com.chocolatecake.indianfood.ui.BaseFragment
 
 
-class CategoriesFragment : Fragment() {
+class CategoriesFragment :BaseFragment<FragmentCategoriesBinding>(){
+
  private lateinit var  _binding:FragmentCategoriesBinding
+    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentCategoriesBinding=
+        FragmentCategoriesBinding::inflate
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding =FragmentCategoriesBinding.inflate(inflater, container, false)
         return _binding.root
 
 
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding.cardView1.setOnClickListener { }
+       binding.cardViewBreakfast.setOnClickListener {
+           val transaction=parentFragmentManager.beginTransaction()
+           transaction.add(R.id.fragment_container,this)
+           transaction.commit()
+
+
+       }
     }
 
 
+
+
+    override fun setUp() {
+    }
+
+    override fun addCallBacks() {
+    }
 }
+
