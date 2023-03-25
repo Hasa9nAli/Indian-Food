197ada130c07eea2ce2d7b91bc08af1ce73048df
Mohammed Alhams <mohammed1212alhams@gmail.com>
2023-03-25 04:57:45 +0300

    refactor to extend the BaseFragment abstract class

--- app/src/main/java/com/chocolatecake/indianfood/ui/CategoriesRecipesFragment.kt
+++ app/src/main/java/com/chocolatecake/indianfood/ui/CategoriesRecipesFragment.kt
@@ -2,61 +2,57 @@ package com.chocolatecake.indianfood.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.chocolatecake.indianfood.R
-import com.chocolatecake.indianfood.databinding.FragmentCategoriesRecpecBinding
+import com.chocolatecake.indianfood.dataSource.CsvDataSource
+import com.chocolatecake.indianfood.dataSource.utils.CsvParser
+import com.chocolatecake.indianfood.databinding.FragmentCategoriesRecipesBinding
 import com.chocolatecake.indianfood.interactor.GetBreakfastRecipesInteractor
 import com.chocolatecake.indianfood.interactor.GetDinnerRecipesInteractor
 import com.chocolatecake.indianfood.interactor.GetLunchRecipesInteractor
 import com.chocolatecake.indianfood.model.Recipe
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CategoriesRecipesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class CategoriesRecipesFragment :BaseFragment<FragmentCategoriesRecpecBinding>(){
-    // TODO: Rename and change types of parameters
+class CategoriesRecipesFragment : BaseFragment<FragmentCategoriesRecipesBinding>() {
+
     private var param1: String? = null
+    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentCategoriesRecipesBinding
+        get() = FragmentCategoriesRecipesBinding::inflate
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun setUp() {
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
         }
-    }
 
-    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentCategoriesRecpecBinding
-        get() = TODO("Not yet implemented")
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories_recpec, container, false)
-    }
+        var recipe: List<Recipe>? = when (param1) {
+            GetBreakfastRecipesInteractor.BREAKFAST ->
+                GetBreakfastRecipesInteractor(
+                    CsvDataSource(
+                        CsvParser(), requireContext()
+                    )
+                ).invoke()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-         var recipe: List<Recipe>? =null
-        when(param1) {
-            GetBreakfastRecipesInteractor.BREAKFAST -> recipe
-            GetLunchRecipesInteractor.LUNCH -> recipe
-            GetDinnerRecipesInteractor.DINNER -> recipe
+            GetLunchRecipesInteractor.LUNCH ->
+                GetLunchRecipesInteractor(
+                    CsvDataSource(
+                        CsvParser(), requireContext()
+                    )
+                ).invoke()
 
+            GetDinnerRecipesInteractor.DINNER ->
+                GetDinnerRecipesInteractor(
+                    CsvDataSource(
+                        CsvParser(), requireContext()
+                    )
+                ).invoke()
+
+            else -> {
+                throw NoSuchElementException()
+            }
         }
-    }
-
-    override fun setUp() {
-        TODO("Not yet implemented")
     }
 
     override fun addCallBacks() {
@@ -65,18 +61,6 @@ class CategoriesRecipesFragment :BaseFragment<FragmentCategoriesRecpecBinding>()
 
 
     companion object {
-
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoriesRecpecFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-
         fun newInstance(param1: String) =
             CategoriesRecipesFragment().apply {
                 arguments = Bundle().apply {
