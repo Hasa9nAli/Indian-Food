84638858b5644c1d1693a3474c57ab42f38f17ae
Saif Alhaider <saifharith90@gmail.com>
2023-03-25 15:40:39 +0300

    add:show more screen adapter

---
+++ app/src/main/java/com/chocolatecake/indianfood/ShowMoreAdapter.kt
@@ -0,0 +1,28 @@
+package com.chocolatecake.indianfood
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.chocolatecake.indianfood.databinding.ShowMoreBinding
+import com.chocolatecake.indianfood.model.Recipe
+
+class ShowMoreAdapter(val recipies: List<Recipe>) :
+    RecyclerView.Adapter<ShowMoreAdapter.ShowMoreViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShowMoreViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.show_more, parent, false)
+        return ShowMoreViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = recipies.size
+
+    override fun onBindViewHolder(holder: ShowMoreViewHolder, position: Int) {
+
+    }
+
+    class ShowMoreViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ShowMoreBinding.bind(view)
+    }
+}
